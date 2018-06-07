b6ca53b6ca26d42a1791a14d0ce3ef2795822ba4
Seph Cordovano <jcordovano@tracerecon.com>
2018-06-06 22:17:00 -0400

    Removed environment restriction from admin user creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 Post.create(first_name: 'Chuck', email: 'chuck@chucknorris.io', caption: 'Chuck Norris can write infinite recursion functions and have them return.', button_title: 'Save to Favorites', image_url: 'http://webiconspng.com/wp-content/uploads/2017/09/Chuck-Norris-PNG-Image-22756.png')
